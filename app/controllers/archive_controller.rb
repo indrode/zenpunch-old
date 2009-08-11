93b1b33f31e1ace5b06f7c374bbfc8dd7cf8ab1b
Indro De <indro.de@gmail.com>
2009-08-11 11:02:15 +0200

    check if Post.id exists or redirect to error page

--- app/controllers/archive_controller.rb
+++ app/controllers/archive_controller.rb
@@ -1,9 +1,13 @@
 class ArchiveController < ApplicationController
-  layout "zen", :except => "comment"
+  layout "zen"
   
   def index
+    if Post.exists?(params[:id])
     @post = Post.find(params[:id])
-    @pagetitle = "Entry #" + @post.id.to_s     
+    @pagetitle = "Entry #" + @post.id.to_s
+    else
+      redirect_to :action => 'oops', :controller => 'zen'
+    end 
   end
   
 end
--- app/controllers/zen_controller.rb
+++ app/controllers/zen_controller.rb
@@ -1,5 +1,5 @@
 class ZenController < ApplicationController
-  layout "zen", :except => { "logs", "zenlog" }
+  layout "zen"
 
   # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
   verify :method => :post, :only => [ :logcomment],
