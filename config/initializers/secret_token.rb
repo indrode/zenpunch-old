331b5eb7b21a97c13eb34445d9078274c5d15741
Indro De <indro.de@gmail.com>
2011-04-09 13:19:33 +0200

    create new rails3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zenpunch::Application.config.secret_token = 'e0c251310eae265ac6b1a3ca3914583a2f57ce70ecf6e834e58802b14ab6c986ecd32db85fb7c9c5016ccc91ffbf84c08c56a60be040896504d0c5db6761abad'
