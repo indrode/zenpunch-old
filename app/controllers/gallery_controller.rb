3716a3dee3eafed0b2bceb61671f7b21bfe89793
Indro De <indro.de@gmail.com>
2011-04-09 13:17:38 +0200

    remove old app

--- app/controllers/gallery_controller.rb
+++
@@ -1,9 +0,0 @@
-class GalleryController < ApplicationController
-  layout "zen"
-  
-  def index
-    @pagetitle = "Gallery"
-    @content = Article.find(:first, :conditions => [ "category = ?", "gallery" ])
-  end
-   
-end
--- app/models/project.rb
+++
@@ -1,3 +0,0 @@
-class Project < ActiveRecord::Base
-  validates_presence_of :title
-end
